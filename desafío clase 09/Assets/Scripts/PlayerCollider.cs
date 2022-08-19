ef2e65353e23c4b0bb363d8351f789ae048c295c
CTMNDZ <73859018+CTMNDZ@users.noreply.github.com>
2022-08-18 22:36:07 -0300

    Corrección

--- desafío clase 09/Assets/Scripts/PlayerCollider.cs
+++ desafío clase 09/Assets/Scripts/PlayerCollider.cs
@@ -5,33 +5,58 @@ using UnityEngine;
 public class PlayerCollider : MonoBehaviour
 {
     
-    private PlayerData playerData;
+    [SerializeField]
+    [Range(0, 2)]
+    private float Cooldown;
+
+    [SerializeField]
+    Transform[] Teleporters;
+
+    private float TimeTouchWall = 0;
+
+    private bool Touch = true;
     // Start is called before the first frame update
     
     private void Start()
     {
-        playerData = GetComponent<PlayerData>();
+
     }
 
-    private void OnCollisionEnter(Collision other)
+    private void OnCollisionEnter(Collision collision)
     {
-  
+        TimeTouchWall = 0;
     }
     private void OnCollisionExit(Collision other)
     {
         //Debug.Log("SALGO DE LA COLISION ->" + other.gameObject.name);
     }
 
-    private void OnCollisionStay(Collision other)
+    private void OnCollisionStay(Collision collision)
     {
+        TimeTouchWall += Time.deltaTime;
+        if(TimeTouchWall >= Cooldown && Touch)
+        {
+            if (collision.gameObject.CompareTag("Pared"))
+            {
+                Touch = false;
+                collision.transform.position = Teleporters[0].position;
+                collision.transform.rotation = Teleporters[0].rotation;
+            }
+            
+        } else if (TimeTouchWall >= Cooldown && Touch == false)
+        {
+            if (collision.gameObject.CompareTag("Pared"))
+            {
+                Touch = true;
+                collision.transform.position = Teleporters[1].position;
+                collision.transform.rotation = Teleporters[1].rotation;
+            }
+        }
 
     }
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Portal"))
-        {
-            playerData.Shrink();
-        }
+
     }
 
     private void OnTriggerExit(Collider other)
--- desafío clase 09/Assets/Scripts/PortalManager.cs
+++ desafío clase 09/Assets/Scripts/PortalManager.cs
@@ -4,21 +4,29 @@ using UnityEngine;
 
 public class PortalManager : MonoBehaviour
 {
-    // Start is called before the first frame update+
-        private PlayerData playerData;
-
-    
-    private void Start()
-    {
-        playerData = GetComponent<PlayerData>();
-    }
-
+    // Start is called before the first frame update
+    private bool CrossingPortal = true;
+    // Start is called before the first frame update
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Portal"))
-        {
-            playerData.Shrink();
-        }
+        ScalePlayer(other);
     }
 
+    void ScalePlayer(Collider Player)
+    {
+        if (CrossingPortal)
+        {
+            CrossingPortal = false;
+            Player.transform.localScale = Player.transform.localScale / 2;
+
+
+        }
+        else
+        if (CrossingPortal == false)
+        {
+            CrossingPortal = false;
+            Player.transform.localScale = Player.transform.localScale * 2;
+
+        }
+    }
 }
